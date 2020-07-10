cfc69c0bc26daf4fe8fc858ea3250136fcc45d46
Probably Brian <ilikerobot+fake@gmail.com>
2020-07-10 11:29:05 -0700

    Update create-readme.rb

--- action/create-readme.rb
+++ action/create-readme.rb
@@ -57,7 +57,7 @@ end
 def build_photo_grid(users)
   lines = []
 
-  users[0...7].map{|k, v| k}.each_slice(4) do |slice|
+  users.map{|k, v| k}.each_slice(4) do |slice|
     header = slice.map { |e| handle_link(e) }.join(" | ").strip
     delimiter = slice.map { |e| "---" }.join(" | ")
     row = slice.map { |e| photo_link(e) }.join(" | ").strip
